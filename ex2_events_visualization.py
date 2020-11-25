121d888b2bf2f1ee810507072c3fbe191fb059c4
Guillermo <guillermo.gallego@ifi.uzh.ch>
2020-11-25 20:04:29 +0100

    clean up. fixed linear voting

--- ex2_events_visualization.py
+++ ex2_events_visualization.py
@@ -4,33 +4,16 @@ Created on Sat Apr 25 19:57:35 2020
 
 Read and write event data.
 
-Visualization of event data in 3D
-- with or without polarity
-- change of viewpoint and movie creation
-
-Conversion of event data into frames (images):
+Conversion of event data into frames (images, 2D):
 - histograms of events
 - thresholded (1 f-stop)
 - brightness increment images
 - time surfaces: exponential decay or average time
 With polarity on the same representation or split by polarity
 
-
-TO DO:
-
-Voxel grid
-- temporal interpolation ("linear")
-
-Evolvig set of points on the image plane?
-Movie of the events (with markers such as +,.)
-
-I think this is more suitable for exercise 3 (image reconstruction?)
-Take two frames, and all the events between the two frames
-Compute the brightness increment image from the events.
-Compute a prediction of the 2nd frame using the 1st frame, the brightness 
-increment image and the estimated contrast threshold?
-Or compute the contrast threshold that minimizes the error between 
-the prediction and the ground truth.
+Visualization of event data in 3D
+- with or without polarity
+- change of viewpoint and movie creation
 
 Write it nicely in utility functions
 
@@ -38,7 +21,6 @@ Write it nicely in utility functions
 """
 
 import os
-#import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -47,22 +29,14 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
 plt.close('all')
 
-"""
-filename = 'slider_depth/images.txt'
-img_timestamps = open(filename, "r")
-
-for line in img_timestamps.readlines():
-    print(line)
-    
-img_timestamps.close()
-"""
 
 # %% Read a file of events and write another file with a subset of them
 filename_sub = 'slider_depth/events_chunk.txt'
 """
+# This is how the file events_chunk.txt was generated from the events.txt file in the IJRR 2017 dataset
 events_raw = open('slider_depth/events.txt', "r")
 events_sub = open(filename_sub, "w")
-# t, x, y, pol
+# format: timestamp, x, y, polarity
 
 for k in range(50000):
     line = events_raw.readline()
@@ -73,7 +47,9 @@ events_raw.close()
 events_sub.close()
 """
 
+
 # %% Read file with a subset of events
+# Simple. There may be more efficient ways.
 def extract_data(filename):
     infile = open(filename, 'r')
     timestamp = []
@@ -82,56 +58,69 @@ def extract_data(filename):
     pol = []
     for line in infile:
         words = line.split()
-        # words[0]: t, words[1]: x
         timestamp.append(float(words[0]))
         x.append(int(words[1]))
         y.append(int(words[2]))
         pol.append(int(words[3]))
     infile.close()
     return timestamp,x,y,pol
-    
+
+# Call the function to read data    
 timestamp, x, y, pol = extract_data(filename_sub)
 
 
-# %% Plot
+# %% Sensor size
 
-# To get the size of the sensor using a grayscale frame (in case of a DAVIS)
+# Get the size of the sensor using a grayscale frame (in case of a DAVIS)
 # filename_frame = 'slider_depth/images/frame_00000000.png'
+# import cv2
 # img = cv2.imread(filename_frame, cv2.IMREAD_GRAYSCALE)
 # print img.shape
 # img = np.zeros(img.shape, np.int)
 
+# For this exercise, we just provide the sensor size (height, width)
 img_size = (180,240)
 
-# Brightness incremet image (Balance of event polarities)
+
+# %% Brightness incremet image (Balance of event polarities)
+num_events = 5000  # Number of events used
+print("Brightness incremet image: numevents = ", num_events)
+
+# Compute image by accumulating polarities.
 img = np.zeros(img_size, np.int)
-num_events = 5000
-print("numevents = ", num_events)
 for i in range(num_events):
-    #timestamp[i]
+    # Need to convert the polarity bit from {0,1} to {-1,+1} and accumulate
     img[y[i],x[i]] += (2*pol[i]-1)
 
+# Display the image in grayscale
 fig = plt.figure()
 fig.suptitle('Balance of event polarities')
-#plt.imshow(img, cmap='gray')
+maxabsval = np.amax(np.abs(img))
+plt.imshow(img, cmap='gray', clim=(-maxabsval,maxabsval))
+plt.colorbar()
+plt.show()
+
+# Same plot as above, but changing the color map
+fig = plt.figure()
+fig.suptitle('Balance of event polarities')
 maxabsval = np.amax(np.abs(img))
 plt.imshow(img, cmap='seismic_r', clim=(-maxabsval,maxabsval))
 plt.colorbar()
 plt.show()
 
 
-# %% Positive and negative events in separate images
+# %% 2D Histograms of events, split by polarity (positive and negative events in separate images)
 img_pos = np.zeros(img_size, np.int)
 img_neg = np.zeros(img_size, np.int)
 for i in range(num_events):
     if (pol[i] > 0):
-        img_pos[y[i],x[i]] += 1
+        img_pos[y[i],x[i]] += 1 # count events
     else:
         img_neg[y[i],x[i]] += 1
 
 fig = plt.figure()
 fig.suptitle('Histogram of positive events')
-plt.imshow(img_pos, cmap='gray_r')
+plt.imshow(img_pos)
 plt.colorbar()
 plt.show()
 
@@ -142,14 +131,16 @@ plt.colorbar()
 plt.show()
 
 
-# %% Thresholded representation
+# %% Thresholded brightness increment image (Ternary image)
 
-# Saturated singl: -1, 0, 1
-# For example, given by the last event polarity at each pixel
+# What if we only use 3 values in the event accumulation image?
+# Saturated signal: -1, 0, 1
+# For example, store the polarity of the last event at each pixel
 img = np.zeros(img_size, np.int)
 for i in range(num_events):
-    img[y[i],x[i]] = (2*pol[i]-1)
+    img[y[i],x[i]] = (2*pol[i]-1)  # no accumulation; overwrite the stored value
 
+# Display the ternary image
 fig = plt.figure()
 fig.suptitle('Last event polarity per pixel')
 plt.imshow(img, cmap='gray')
@@ -158,9 +149,12 @@ plt.colorbar()
 plt.show()
 
 
-# %% Time surface (or time map, or SAE)
+
+
+# _____________________________________________________________________________
+# %% Time surface (or time map, or SAE="Surface of Active Events")
 num_events = len(timestamp)
-print("numevents = ", num_events)
+print("Time surface: numevents = ", num_events)
 
 img = np.zeros(img_size, np.float32)
 t_ref = timestamp[-1] # time of the last event in the packet
@@ -196,10 +190,8 @@ plt.imshow(sae_neg)
 plt.colorbar()
 plt.show()
 
-# Using color (Red/blue) --> colormap seismic
 
-
-# %% Time surface (or time map, or SAE), separated by polarity
+# %% Time surface (or time map, or SAE), using polarity as sign of the time map
 sae = np.zeros(img_size, np.float32)
 for i in range(num_events):
     if (pol[i] > 0):
@@ -208,28 +200,29 @@ for i in range(num_events):
         sae[y[i],x[i]] = -np.exp(-(t_ref-timestamp[i]) / tau)
 
 fig = plt.figure()
-fig.suptitle('Time surface (exp decay). Both polarities, signed')
-#plt.imshow(sae)
-plt.imshow(sae, cmap='seismic')
+fig.suptitle('Time surface (exp decay), using polarity as sign')
+plt.imshow(sae, cmap='seismic') # using color (Red/blue)
 plt.colorbar()
 plt.show()
 
 
-# %% Balance of 
+# %% "Balance of time surfaces"
+# Accumulate exponential decays using polarity as sign of the time map
 sae = np.zeros(img_size, np.float32)
 for i in range(num_events):
     if (pol[i] > 0):
-        sae[y[i],x[i]] = sae[y[i],x[i]] + np.exp(-(t_ref-timestamp[i]) / tau)
+        sae[y[i],x[i]] += np.exp(-(t_ref-timestamp[i]) / tau)
     else:
-        sae[y[i],x[i]] = sae[y[i],x[i]] - np.exp(-(t_ref-timestamp[i]) / tau)
+        sae[y[i],x[i]] -= np.exp(-(t_ref-timestamp[i]) / tau)
 
 fig = plt.figure()
-fig.suptitle('Time surface (exp decay). Balance of both polarities.')
+fig.suptitle('Time surface (exp decay), balance of both polarities')
 #plt.imshow(sae)
 maxabsval = np.amax(np.abs(sae))
 plt.imshow(sae, cmap='seismic', clim=(-maxabsval,maxabsval))
 plt.colorbar()
 plt.show()
+
 
 # %% Average timestamp per pixel
 sae = np.zeros(img_size, np.float32)
@@ -239,7 +232,7 @@ for i in range(num_events):
     count[y[i],x[i]] += 1
     
 # Compute per-pixel average if count at the pixel is >1
-count [count < 1] = 1
+count [count < 1] = 1  # to avoid division by zero
 sae = sae / count
 
 fig = plt.figure()
@@ -278,12 +271,15 @@ plt.imshow(sae_neg)
 plt.colorbar()
 plt.show()
 
-# %% 3D plot (evemts and frames)
-# time axis horizontally
+
+
+
+# _____________________________________________________________________________
+# %% 3D plot 
+# Time axis in horizontal position
 
 m = 2000 # Number of points to plot
-
-#plt.close('all')
+print("Space-time plot and movie: numevents = ", m)
 
 # Plot without polarity
 fig = plt.figure()
@@ -293,10 +289,8 @@ ax.scatter(x[:m], timestamp[:m], y[:m], marker='.', c='b')
 ax.set_xlabel('x [pix]')
 ax.set_ylabel('time [s]')
 ax.set_zlabel('y [pix] ')
-ax.view_init(azim=-90, elev=-180)
+ax.view_init(azim=-90, elev=-180) # Change viewpoint with the mouse, for example
 plt.show()
-
-# Change viewpoint with the mouse, for example
 
 
 # %% Plot each polarity with a different color (red / blue)
@@ -307,7 +301,6 @@ ynp = np.asarray(y[:m])
 tnp = np.asarray(timestamp[:m])
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-#ax.set_aspect('equal') # only works for time in Z axis
 ax.scatter(xnp[idx_pos], tnp[idx_pos], ynp[idx_pos], marker='.', c='b')
 ax.scatter(xnp[idx_neg], tnp[idx_neg], ynp[idx_neg], marker='.', c='r')
 ax.set(xlabel='x [pix]', ylabel='time [s]', zlabel='y [pix]')
@@ -315,8 +308,8 @@ ax.view_init(azim=-90, elev=-180)
 plt.show()
 
 
-# %% Transition: from viewpoint [-140,-60] to [-90,-90]
-num_interp_viewpoints = 60
+# %% Transition between two viewpoints
+num_interp_viewpoints = 60 # number of interpolated viewpoints
 ele = np.linspace(-150,-180, num=num_interp_viewpoints)
 azi = np.linspace( -50, -90, num=num_interp_viewpoints)
 
@@ -332,53 +325,46 @@ for ii in xrange(0,num_interp_viewpoints):
     ax.view_init(azim=azi[ii], elev=ele[ii])
     plt.savefig(dirName + "/movie%04d.png" % ii)
 
-# %% Create a movii using ffmpeg static build (https://johnvansickle.com/ffmpeg/)
+# %% Create a movie using ffmpeg static build (https://johnvansickle.com/ffmpeg/)
 # Video coding options, such as lossless: https://trac.ffmpeg.org/wiki/Encode/H.264
 def createMovie():
-    os.system("/home/ggb/Downloads/ffmpeg-4.0.1-64bit-static/ffmpeg -r 20 -i " 
-    + dirName  + "/movie%04d.png -c:v libx264 -crf 0 -y movie.mp4")
+    os.system("/home/ggb/Downloads/ffmpeg-4.2.2-i686-static/ffmpeg -r 20 -i " 
+    + dirName  + "/movie%04d.png -c:v libx264 -crf 0 -y movie_new.mp4")
 
-#createMovie()
-
-
-# %% Generate video, at constant number of events or constant time intervals
+# Call the function to create the movie
+createMovie()
 
 
-# %% Try to reason about the events
-# How many per pixel per second? (average or histogram)
 
 
+# _____________________________________________________________________________
 # %% Voxel grid
 
-#plt.close('all')
-
-# First, count histogram
-# Input: x,y,timestamp,pol
-
-# Just use the first m events
-
-num_bins = 5
+num_bins = 10
+print("Number of time bins = ", num_bins)
 
 t_max = np.amax(np.asarray(timestamp[:m]))
 t_min = np.amin(np.asarray(timestamp[:m]))
 t_range = t_max - t_min
-dt_cell = t_range / num_bins
-t_edges = np.linspace(t_min,t_max,num_bins+1) # boundaries of the cells; not needed
+dt_bin = t_range / num_bins # size of the time bins (bins)
+t_edges = np.linspace(t_min,t_max,num_bins+1) # Boundaries of the bins
 
-# Compute the 3D histogram
-# "Zero-th order or nearest neighbor voting"
+# Compute 3D histogram of events manually with a loop
+# ("Zero-th order or nearest neighbor voting")
 hist3d = np.zeros(img.shape+(num_bins,), np.int)
 for ii in xrange(m):
-    idx_t = int( (timestamp[ii]-t_min) / dt_cell )
+    idx_t = int( (timestamp[ii]-t_min) / dt_bin )
     if idx_t >= num_bins:
         idx_t = num_bins-1 # only one element (the last one)
     hist3d[y[ii],x[ii],idx_t] += 1
 
 # Checks:
-#print hist3d.shape
-#print np.sum(hist3d) # This should equal the number of votes
+print("hist3d")
+print hist3d.shape
+print np.sum(hist3d) # This should equal the number of votes
 
-# %% Using numpy function histogramdd
+ 
+# %% Compute 3D histogram of events using numpy function histogramdd
 # https://docs.scipy.org/doc/numpy/reference/generated/numpy.histogramdd.html#numpy.histogramdd
 
 # Specify bin edges in each dimension
@@ -388,129 +374,92 @@ yxt = np.transpose(np.array([y[:m], x[:m], timestamp[:m]]))
 hist3dd, edges = np.histogramdd(yxt, bins=bin_edges)
 
 # Checks
-#print np.sum(hist3dd)
-#print np.linalg.norm( hist3dd - hist3d)
+print("\nhist3dd")
+print("min = ", np.min(hist3dd))
+print("max = ", np.max(hist3dd))
+print np.sum(hist3dd)
+print np.linalg.norm( hist3dd - hist3d) # Check: zero if both histograms are equal
+print("Ratio of occupied bins = ", np.sum(hist3dd > 0) / float(np.prod(hist3dd.shape)) )
 
-"""
-# Debugging. Plot error images
-idx = 0
-fig = plt.figure()
-plt.imshow(hist3d[:,:,idx])
-plt.colorbar()
-plt.show()
 
-fig = plt.figure()
-plt.imshow(hist3dd[:,:,idx] - hist3d[:,:,idx])
-plt.colorbar()
-plt.show()
-"""
-
-# %% Plot
+# Plot of the 3D histogram. Empty cells are transparent (not displayed)
 # Example: https://matplotlib.org/3.1.1/gallery/mplot3d/voxels_rgb.html#sphx-glr-gallery-mplot3d-voxels-rgb-py
 
+fig = plt.figure()
+fig.suptitle('3D histogram (voxel grid), zero-th order voting')
+ax = fig.gca(projection='3d')
 # prepare some coordinates
 r, g, b = np.indices((img_size[0]+1,img_size[1]+1,num_bins+1))
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.voxels(r,g,b, hist3d)
-ax.set(xlabel='y', ylabel='x', zlabel='time bin')
-plt.show()
-
-# It is smart: there is no need to swap the data to plot with reordered axes
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.voxels(g,b,r, hist3d)
+ax.voxels(g,b,r, hist3d) # No need to swap the data to plot with reordered axes
 ax.set(xlabel='x', ylabel='time bin', zlabel='y')
-#ax.view_init(azim=-90, elev=-180)
-#ax.view_init(azim=-63, elev=-145)
-plt.show()
-
-# %%
-colors = np.zeros(hist3d.shape + (3,))
-colors[..., 0] = hist3d
-colors[..., 1] = hist3d
-colors[..., 2] = hist3d
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.voxels(g,b,r, hist3d, facecolors=colors)
-ax.set(xlabel='x', ylabel='time bin', zlabel='y')
-#ax.view_init(azim=-90, elev=-180)
-ax.view_init(azim=-63, elev=-145)
+ax.view_init(azim=-90, elev=-180) # edge-on, along time axis
+#ax.view_init(azim=-63, elev=-145) # oblique viewpoint
 plt.show()
 
 
-# %% Compute interpolated histogram
+# %% Compute interpolated 3D histogram (voxel grid)
 hist3d_interp = np.zeros(img.shape+(num_bins,), np.float64)
 for ii in xrange(m-1):
-    tn = (timestamp[ii] - t_min) / dt_cell
-    ti = int(tn)
-    dt = tn - ti
-    # Voting on two adjacent cells
-    hist3d_interp[y[ii],x[ii],ti  ] += 1. - dt
-    if ti < num_bins-1:
-        hist3d_interp[y[ii],x[ii],ti+1] += dt
+    tn = (timestamp[ii] - t_min) / dt_bin # normalized time, in [0,num_bins]
+    ti = np.floor(tn-0.5) # index of the left bin
+    dt = (tn-0.5) - ti    # delta fraction
+    # Voting on two adjacent bins
+    if ti >=0 :
+        hist3d_interp[y[ii],x[ii],int(ti)  ] += 1. - dt
+    if ti < num_bins-1 :
+        hist3d_interp[y[ii],x[ii],int(ti)+1] += dt
 
 # Checks
+print("\nhist3d_interp")
+print("min = ", np.min(hist3d_interp))
+print("max = ", np.max(hist3d_interp))
 print np.sum(hist3d_interp)
 # Some votes are lost because of the missing last layer
 print np.linalg.norm( hist3d - hist3d_interp)
+print("Ratio of occupied bins = ", np.sum(hist3d_interp > 0) / float(np.prod(hist3d_interp.shape)) )
 
-# Debugging
-"""
-idx = 2
-fig = plt.figure()
-plt.imshow(hist3d_interp[:,:,idx])
-plt.colorbar()
-plt.show()
-
-fig = plt.figure()
-plt.imshow(hist3d[:,:,idx])
-plt.colorbar()
-plt.show()
-
-fig = plt.figure()
-plt.imshow(hist3d_interp[:,:,idx] - hist3d[:,:,idx])
-plt.colorbar()
-plt.show()
-"""
-
-# %% Plot voxel grid
-
+# Plot voxel grid
 colors = np.zeros(hist3d_interp.shape + (3,))
-colors[..., 0] = hist3d_interp
-colors[..., 1] = hist3d_interp
-colors[..., 2] = hist3d_interp
+tmp = hist3d_interp/np.amax(hist3d_interp) # normalize in [0,1]
+colors[..., 0] = tmp
+colors[..., 1] = tmp
+colors[..., 2] = tmp
 
 fig = plt.figure()
+fig.suptitle('Interpolated 3D histogram (voxel grid)')
 ax = fig.gca(projection='3d')
 ax.voxels(g,b,r, hist3d_interp, facecolors=colors)
 ax.set(xlabel='x', ylabel='time bin', zlabel='y')
-#ax.view_init(azim=-90, elev=-180)
 ax.view_init(azim=-63, elev=-145)
 plt.show()
 
+# %% A different visualization viewpoint
+ax.view_init(azim=-90, elev=-180) # edge-on, along time axis
+plt.show()
 
 
-# %%
-
+# %% Compute interpolated 3D histogram (voxel grid) using polarity
 hist3d_interp_pol = np.zeros(img.shape+(num_bins,), np.float64)
 for ii in xrange(m-1):
-    tn = (timestamp[ii] - t_min) / dt_cell
-    ti = int(tn)
-    dt = tn - ti
-    # Voting on two adjacent cells
-    hist3d_interp_pol[y[ii],x[ii],ti  ] += (1. - dt) * (2*pol[ii]-1)
-    if ti < num_bins-1:
-        hist3d_interp_pol[y[ii],x[ii],ti+1] += dt * (2*pol[ii]-1)
+    tn = (timestamp[ii] - t_min) / dt_bin # normalized time, in [0,num_bins]
+    ti = np.floor(tn-0.5) # index of the left bin
+    dt = (tn-0.5) - ti    # delta fraction
+    # Voting on two adjacent bins
+    if ti >=0 :
+        hist3d_interp_pol[y[ii],x[ii],int(ti)  ] += (1. - dt) * (2*pol[ii]-1)
+    if ti < num_bins-1 :
+        hist3d_interp_pol[y[ii],x[ii],int(ti)+1] += dt * (2*pol[ii]-1)
 
 # Checks
-print np.sum(hist3d_interp)
 # Some votes are lost because of the missing last layer
-print np.linalg.norm( hist3d - hist3d_interp)
+print("\nhist3d_interp_pol")
+print("min = ", np.min(hist3d_interp_pol))
+print("max = ", np.max(hist3d_interp_pol))
+print np.sum(np.abs(hist3d_interp_pol))
+print("Ratio of occupied bins = ", np.sum(np.abs(hist3d_interp_pol) > 0) / float(np.prod(hist3d_interp_pol.shape)) )
 
-# %%
+# Plot interpolated voxel grid using polarity
+# Normalize the symmetric range to [0,1]
 maxabsval = np.amax(np.abs(hist3d_interp_pol))
 colors = np.zeros(hist3d_interp_pol.shape + (3,))
 tmp = (hist3d_interp_pol + maxabsval)/(2*maxabsval)
@@ -519,9 +468,13 @@ colors[..., 1] = tmp
 colors[..., 2] = tmp
 
 fig = plt.figure()
+fig.suptitle('Interpolated 3D histogram (voxel grid), including polarity')
 ax = fig.gca(projection='3d')
 ax.voxels(g,b,r, hist3d_interp_pol, facecolors=colors)
 ax.set(xlabel='x', ylabel='time bin', zlabel='y')
-#ax.view_init(azim=-90, elev=-180)
 ax.view_init(azim=-63, elev=-145)
+plt.show()
+
+# %% Better visualization viewpoint to see positive and negative edges
+ax.view_init(azim=-90, elev=-180) # edge-on, along time axis
 plt.show()
